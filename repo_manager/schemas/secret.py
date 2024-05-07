293f8916dd7aaac649182dfbc15a473f6e3e8d16
shiro <24359398+actuarysailor@users.noreply.github.com>
2024-05-07 09:51:23 -0700

    Update secret.py

--- repo_manager/schemas/secret.py
+++ repo_manager/schemas/secret.py
@@ -2,8 +2,7 @@ import os
 from typing import Optional
 
 from pydantic import BaseModel  # pylint: disable=E0611
-from pydantic import Field
-from pydantic import validator
+from pydantic import Field, field_validator
 
 OptBool = Optional[bool]
 OptStr = Optional[str]
@@ -13,29 +12,32 @@ class SecretEnvError(Exception): ...
 
 
 class Secret(BaseModel):
-    type: OptStr = Field(None, description="Type of secret, can be `dependabot` or `actions`")
-    key: OptStr = Field(None, description="Secret's name.")
-    env: OptStr = Field(None, description="Environment variable to pull the secret from")
-    value: OptStr = Field(None, description="Value to set this secret to")
+    type: str = Field(
+        "actions",
+        description="Type of secret, can be `dependabot` or `actions` or an `environment` path",
+    )
+    key: str = Field(None, description="Secret's name.")
+    env: OptStr = Field(
+        None, description="Environment variable to pull the secret from"
+    )
+    value: OptStr = Field(
+        None, description="Value to set this secret to", validate_default=True
+    )
     required: OptBool = Field(
         True,
         description="Setting a value as not required allows you to not pass in an env var without causing an error",
     )
     exists: OptBool = Field(True, description="Set to false to delete a secret")
 
-    @validator("type")
-    def validate_type(cls, v):
-        if v not in ["dependabot", "actions"]:
-            raise ValueError("Secret type must be either `dependabot` or `actions`")
-        return v
-
-    @validator("value", always=True)
+    @field_validator("value")
     def validate_value(cls, v, values) -> OptStr:
         if v is None:
             return None
 
-        if values["env"] is not None:
-            raise ValueError("Cannot set an env and a value in the same secret, remove one.")
+        if values.data["env"] is not None:
+            raise ValueError(
+                "Cannot set an env and a value in the same secret, remove one."
+            )
 
         return v
 
