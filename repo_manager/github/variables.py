8c4acf54764462f9849bf09628a87d33e4d9e27d
shiro <24359398+actuarysailor@users.noreply.github.com>
2024-05-13 16:32:35 -0400

    Update variables.py

--- repo_manager/github/variables.py
+++ repo_manager/github/variables.py
@@ -16,13 +16,9 @@ def diff_option(key: str, expected: Any, repo_value: Any) -> str | None:
 
 
 def _get_repo_variables(repo: Repository, path: str = "actions") -> Any:
-    status, headers, raw_data = repo._requester.requestJson(
-        "GET", f"{repo.url}/{path}/variables"
-    )
+    status, headers, raw_data = repo._requester.requestJson("GET", f"{repo.url}/{path}/variables")
     if status != 200:
-        raise Exception(
-            f"Unable to get repo's variables. Status: {status}. Error: {json.loads(raw_data)['message']}"
-        )
+        raise Exception(f"Unable to get repo's variables. Status: {status}. Error: {json.loads(raw_data)['message']}")
     try:
         variable_data = json.loads(raw_data)
     except json.JSONDecodeError as exc:
@@ -39,9 +35,7 @@ def _get_repo_variable_dict(repo: Repository, path: str = "actions") -> dict[str
     return {variable["name"]: variable for variable in _get_repo_variables(repo, path)}
 
 
-def create_variable(
-    repo: Repository, variable_name: str, value: str, path: str = "actions"
-) -> bool:
+def create_variable(repo: Repository, variable_name: str, value: str, path: str = "actions") -> bool:
     """
     :calls: `POST /repos/{owner}/{repo}/{path}/variables
     <https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28>`_
@@ -51,19 +45,13 @@ def create_variable(
     :path: string
     """
     put_parameters = {"name": variable_name, "value": value}
-    status, headers, data = repo._requester.requestJson(
-        "POST", f"{repo.url}/{path}/variables", input=put_parameters
-    )
+    status, headers, data = repo._requester.requestJson("POST", f"{repo.url}/{path}/variables", input=put_parameters)
     if status not in {201}:
-        raise Exception(
-            f"Unable to create variable: {variable_name}. Error: {json.loads(data)['message']}"
-        )
+        raise Exception(f"Unable to create variable: {variable_name}. Error: {json.loads(data)['message']}")
     return True
 
 
-def update_variable(
-    repo: Repository, variable_name: str, value: str, path: str = "actions"
-) -> bool:
+def update_variable(repo: Repository, variable_name: str, value: str, path: str = "actions") -> bool:
     """
     :calls: `PATCH /repos/{owner}/{repo}/{path}/variables/{variable_name}
     <https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28>`_
@@ -77,15 +65,11 @@ def update_variable(
         "PATCH", f"{repo.url}/{path}/variables/{variable_name}", input=put_parameters
     )
     if status not in {204}:
-        raise Exception(
-            f"Unable to update variable: {variable_name}. Error: {json.loads(data)['message']}"
-        )
+        raise Exception(f"Unable to update variable: {variable_name}. Error: {json.loads(data)['message']}")
     return True
 
 
-def delete_variable(
-    repo: Repository, variable_name: str, path: str = "actions"
-) -> bool:
+def delete_variable(repo: Repository, variable_name: str, path: str = "actions") -> bool:
     """
     :calls: `DELETE /repos/{owner}/{repo}/{path}/variables/{variable_name}
     <https://docs.github.com/en/rest/actions/variables?apiVersion=2022-11-28>`_
@@ -103,9 +87,7 @@ def delete_variable(
     return status == 204
 
 
-def check_variables(
-    repo: Repository, variables: list[Secret]
-) -> tuple[bool, dict[str, list[str] | dict[str, Any]]]:
+def check_variables(repo: Repository, variables: list[Secret]) -> tuple[bool, dict[str, list[str] | dict[str, Any]]]:
     """Checks a repo's secrets vs our expected settings
 
     Args:
@@ -119,42 +101,29 @@ def check_variables(
     if any(filter(lambda variable: variable.type == "actions", variables)):
         repo_dict.update(_get_repo_variable_dict(repo))
     if any(filter(lambda variable: variable.type not in {"actions"}, variables)):
-        first_variable = next(
-            filter(lambda variable: variable.type not in {"actions"}, variables), None
-        )
+        first_variable = next(filter(lambda variable: variable.type not in {"actions"}, variables), None)
         if first_variable is not None:
             repo_dict.update(_get_repo_variable_dict(repo, first_variable.type))
     config_dict = {variable.key: variable for variable in variables}
     repo_variable_names = {variable for variable in repo_dict.keys()}
 
-    expected_variables_names = {
-        variable.key for variable in filter(lambda variable: variable.exists, variables)
-    }
+    expected_variables_names = {variable.key for variable in filter(lambda variable: variable.exists, variables)}
     diff = {
         "missing": list(expected_variables_names - repo_variable_names),
         "extra": list(
             repo_variable_names.intersection(
-                {
-                    variable.key
-                    for variable in filter(
-                        lambda variable: variable.exists is False, variables
-                    )
-                }
+                {variable.key for variable in filter(lambda variable: variable.exists is False, variables)}
             )
         ),
         "diff": {},
     }
 
-    variables_to_check_values_on = list(
-        expected_variables_names.intersection(repo_variable_names)
-    )
+    variables_to_check_values_on = list(expected_variables_names.intersection(repo_variable_names))
     for variable_name in variables_to_check_values_on:
         config_var = config_dict.get(variable_name, None)
         repo_var = repo_dict.get(variable_name, None)
         if config_var.value != repo_var["value"]:
-            diff["diff"][variable_name] = diff_option(
-                variable_name, config_var.value, repo_var["value"]
-            )
+            diff["diff"][variable_name] = diff_option(variable_name, config_var.value, repo_var["value"])
 
     if len(diff["missing"]) + len(diff["extra"]) + len(diff["diff"]) > 0:
         return False, diff
